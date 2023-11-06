0746959591501b430344705b48c0254eddfcb12f
Ashlee Sanjay <ashleedavis09@yahoo.com>
2023-11-07 09:34:55 +1300

    db port name changed

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const db = mysql.createConnection({
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
-  port: process.env.DB_PORT,
+  db_port: process.env.DB_PORT,
 });
 db.connect((err) => {
   if (err) {
